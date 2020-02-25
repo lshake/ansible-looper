824dfc967898ef2df3b2fb3c423d8ef321daea0a
Lee Shakespeare <lee@shakey.org>
2020-02-25 10:53:19 -0500

    Make artificats work, remove unused function

--- run-ha.py
+++ run-ha.py
@@ -7,12 +7,13 @@ import time
 import random
 from termcolor import colored
 
-test_directory = "/Users/lshakesp/projects/ansible/ansible-runner/ha_tests"
+test_directory = "./ha_tests"
 ansible_tests_list = []
 ansible_run_list = []
 tests_list = []
 iterations = 20
 maxfailures = 3
+keepartifacts = 5
 debug = False
 
 
@@ -27,36 +28,18 @@ def get_tests(test_directory):
     return(tests)
 
 
-def launch_ansible_test(test_to_launch, test_directory, test_type):
+def launch_ansible_test(test_to_launch, test_directory, test_type, invocation, failure_count):
     (t, r) = ansible_runner.interface.run_async(
         private_data_dir=test_directory + '/' + test_to_launch,
-        playbook=test_type + '.yml')
+        playbook=test_type + '.yml',
+        rotate_artifacts=keepartifacts,
+        ident=test_to_launch + '_' + str(invocation) + '_' + str(failure_count))
     return({
         'thread': t,
         'runner': r,
         'test': test_to_launch,
         'test_directory': test_directory
     })
-
-
-def launch_ansible_tests(list_of_tests, test_type):
-    running_tests = []
-    for test in list_of_tests:
-        launched_test = launch_ansible_test(test, test_directory, test_type)
-        running_tests.append({
-            'thread': launched_test['thread'],
-            'runner': launched_test['runner'],
-            'test_name': launched_test['test'],
-            'test_directory': launched_test['test_directory'],
-            'test_type': test_type,
-            'iteration': 1,
-            'failures': 0
-        })
-        print(colored("Launching : {} - {} :{}: iteration {}".format(
-            test, launched_test['runner'].status,
-            test_type, 1), 'yellow')
-        )
-    return(running_tests)
 
 
 def convert_test_list_to_dict(list_of_tests, test_type):
@@ -72,7 +55,7 @@ def convert_test_list_to_dict(list_of_tests, test_type):
 def launch_test_sequence(run_list):
     for test in run_list:
         launched_test = launch_ansible_test(
-            test['test_name'], test_directory, test['test_type'])
+            test['test_name'], test_directory, test['test_type'], 1, 0)
         test['thread'] = launched_test['thread']
         test['runner'] = launched_test['runner']
         print(colored("Launching : {} - {}".format(
